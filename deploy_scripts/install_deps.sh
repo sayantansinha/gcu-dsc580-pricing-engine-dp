a7eece886007e7a9350351a6c4aed037e7e0502d
ssinha3 <sayantan_sinha@spe.sony.com>
2025-11-14 20:08:36 -0800

    DSC-580 Topic 7 Assignment - Deployment

--- deploy_scripts/install_deps.sh
+++ deploy_scripts/install_deps.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 cd /opt/ppe/current
+
 # Python
 command -v python3.11 >/dev/null 2>&1 || dnf -y install python3.11 python3.11-pip
 /usr/bin/python3.11 -m pip install --upgrade pip
 if [ -f requirements.txt ]; then
+  printf "Installing dependencies"
   /usr/bin/python3.11 -m pip install -r requirements.txt
 fi
 
 # systemd unit (only if you keep it versioned)
+printf "PPE service"
 install -m 0644 deploy_scripts/ppe.service /etc/systemd/system/ppe.service || true
 systemctl daemon-reload
