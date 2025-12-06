01d9f3fe66a1b59c851ac520b9e82156f1a9dba0
ssinha3 <sayantan_sinha@spe.sony.com>
2025-12-05 22:57:12 -0800

    DSC-580 Topic 7 Assignment - interim deployment step

--- infra/github_oidc.tf
+++ infra/github_oidc.tf
@@ -1,75 +1,75 @@
-# OIDC provider for GitHub Actions (once per account)
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"] # GitHub's
-}
-
-# Role that GitHub Actions will assume to deploy
-resource "aws_iam_role" "gha_deployer" {
-  name = "${local.app_prefix}-gha-deployer"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = { Federated = aws_iam_openid_connect_provider.github.arn },
-        Action    = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          },
-          StringLike = {
-            # limit to your repo and default branches/tags
-            "token.actions.githubusercontent.com:sub" = [
-              "repo:sayantansinha/gcu-dsc580-pricing-engine-dp:ref:refs/heads/aws_deploy",
-              "repo:sayantansinha/gcu-dsc580-pricing-engine-dp:ref:refs/tags/*"
-            ]
-          }
-        }
-      }
-    ]
-  })
-  tags = local.tags
-}
-
-# Policy for uploading artifact to S3 and triggering CodeDeploy
-resource "aws_iam_policy" "gha_deploy_policy" {
-  name = "${local.app_prefix}-gha-deploy"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid    = "PutArtifact"
-        Effect = "Allow"
-        Action = [
-          "s3:PutObject",
-          "s3:PutObjectAcl"
-        ]
-        Resource = [
-          "arn:aws:s3:::${local.deploy_artifact_bucket}/releases/*"
-        ]
-      },
-      {
-        Sid    = "CodeDeployDeploy"
-        Effect = "Allow"
-        Action = [
-          "codedeploy:CreateDeployment",
-          "codedeploy:RegisterApplicationRevision",
-          "codedeploy:GetDeployment",
-          "codedeploy:GetDeploymentConfig",
-          "codedeploy:GetApplication",
-          "codedeploy:GetDeploymentGroup"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-  tags = local.tags
-}
-
-
-resource "aws_iam_role_policy_attachment" "gha_attach" {
-  role       = aws_iam_role.gha_deployer.name
-  policy_arn = aws_iam_policy.gha_deploy_policy.arn
-}
+# # OIDC provider for GitHub Actions (once per account)
+# resource "aws_iam_openid_connect_provider" "github" {
+#   url             = "https://token.actions.githubusercontent.com"
+#   client_id_list  = ["sts.amazonaws.com"]
+#   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"] # GitHub's
+# }
+#
+# # Role that GitHub Actions will assume to deploy
+# resource "aws_iam_role" "gha_deployer" {
+#   name = "${local.app_prefix}-gha-deployer"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect    = "Allow",
+#         Principal = { Federated = aws_iam_openid_connect_provider.github.arn },
+#         Action    = "sts:AssumeRoleWithWebIdentity",
+#         Condition = {
+#           StringEquals = {
+#             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+#           },
+#           StringLike = {
+#             # limit to your repo and default branches/tags
+#             "token.actions.githubusercontent.com:sub" = [
+#               "repo:sayantansinha/gcu-dsc580-pricing-engine-dp:ref:refs/heads/aws_deploy",
+#               "repo:sayantansinha/gcu-dsc580-pricing-engine-dp:ref:refs/tags/*"
+#             ]
+#           }
+#         }
+#       }
+#     ]
+#   })
+#   tags = local.tags
+# }
+#
+# # Policy for uploading artifact to S3 and triggering CodeDeploy
+# resource "aws_iam_policy" "gha_deploy_policy" {
+#   name = "${local.app_prefix}-gha-deploy"
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Sid    = "PutArtifact"
+#         Effect = "Allow"
+#         Action = [
+#           "s3:PutObject",
+#           "s3:PutObjectAcl"
+#         ]
+#         Resource = [
+#           "arn:aws:s3:::${local.deploy_artifact_bucket}/releases/*"
+#         ]
+#       },
+#       {
+#         Sid    = "CodeDeployDeploy"
+#         Effect = "Allow"
+#         Action = [
+#           "codedeploy:CreateDeployment",
+#           "codedeploy:RegisterApplicationRevision",
+#           "codedeploy:GetDeployment",
+#           "codedeploy:GetDeploymentConfig",
+#           "codedeploy:GetApplication",
+#           "codedeploy:GetDeploymentGroup"
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+#   tags = local.tags
+# }
+#
+#
+# resource "aws_iam_role_policy_attachment" "gha_attach" {
+#   role       = aws_iam_role.gha_deployer.name
+#   policy_arn = aws_iam_policy.gha_deploy_policy.arn
+# }
